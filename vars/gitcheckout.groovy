8670e2665abca1354ec65aac73a37191ae0f2deb
shivashankarbukka402 <shivashankarbukka402@gmail.com>
2025-10-09 18:28:35 +0530

    Update gitcheckout.groovy

--- vars/gitcheckout.groovy
+++ vars/gitcheckout.groovy
@@ -1,4 +1,8 @@
-# Git check Jenkins Library
-def call() {
-  echo "This is git checkout shared library
+def call(String git_branch = null, String git_credentailes = null, String git_url = null) {
+  echo "This is git checkout shared library"
+  checkout{[$class: 'GitSCM',
+            branches: [[name: "${git_branch}"]],
+            userRemoteConfigs: [[credentialsId: "${git_credentails}",
+            url: "${git_url}"]]])
+  sh 'pwd; ls -lrt'
 }
