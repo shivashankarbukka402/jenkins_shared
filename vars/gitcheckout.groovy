fe7dfc50e3c7e5341b51e698bceeaa44ee87e8ec
shankar Hampi <shankarshankar90877@gamil.com>
2025-11-24 13:05:43 +0530

    update the jenkins_shared file

--- vars/gitcheckout.groovy
+++ vars/gitcheckout.groovy
@@ -1,8 +1,10 @@
-def call(String git_branch = main, String git_credentailes = null, String git_url = null) {
-  echo "This is git checkout shared library"
-  checkout{[$class: 'GitSCM',
-            branches: [[name: "${git_branch}"]],
-            userRemoteConfigs: [[credentialsId: "${git_credentails}",
-            url: "${git_url}"]]])
-  sh 'pwd; ls -lrt'
+def call(String git_branch = 'main', String git_credentails = null, String git_url = null) {
+     echo "This is gitcheck shared library"
+     checkout([$class: 'GitSCM',
+               branches: [[name: "${git_branch}"]],
+               userRemoteConfigs: [[credentialsId: "${git_credentails}",
+               url: "${git_url}"]]])
+     echo "Git checkout to ${git_url} is success"
 }
+
+// gitCheckout('main', 'github_creds', 'repo_url')
